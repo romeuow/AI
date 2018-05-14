82a2d20d06112f7c0517d18d368da4935cf08253
Romeu Junio Cunha de Oliveira <romeu.oliveira@paraopeba.grad.dcc.ufmg.br>
2018-05-14 08:24:54 -0300

    Finalizado! 	modified:   tp1/aestrela.sh 	modified:   tp1/bg.sh 	modified:   tp1/ids.sh 	modified:   tp1/src/BuildMap.py 	modified:   tp1/ucs.sh

--- tp1/src/BuildMap.py
+++ tp1/src/BuildMap.py
@@ -4,7 +4,6 @@ import sys
 class BuildMapClass:
     @staticmethod
     def build_map(mapa):
-        out = open("mapaloco.map",'w')
         my_map = open(mapa, "rb")
         my_map.readline()
         my_map.read(7)
@@ -16,8 +15,5 @@ class BuildMapClass:
         for i in range(0, line_num):
             for j in range(0, column_num):
                 hash_map.update({(i, j): my_map.read(1).decode('utf-8')})
-                out.write(hash_map[(i, j)])
             my_map.seek(1, 1)
-            out.write('\n')
-        out.close()
         return hash_map
--- tp1/ucs.sh
+++ tp1/ucs.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 if [ $# -eq 5 ]; then
 	MAPA=$(pwd)/$1
-	cd src
-	python3 Main.py ucs $MAPA $2 $3 $4 $5 $6
+	python3 src/Main.py ucs $MAPA $2 $3 $4 $5 $6
 
 else
 	echo Incorrect parameters
