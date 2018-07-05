2a3f91bb87efe69c690121e41fffe8f44784800c
Romeu Oliveira <romeu.oliveira@tatic.net>
2018-07-05 08:32:29 -0300

    Arquivo pi.txt

--- tp2/src/MDP.py
+++ tp2/src/MDP.py
@@ -1,7 +1,3 @@
-from utils import *
-from Movement import MovementClass
-import numpy as np
-
 class MDPClass:
 	"""A Markov Decision Process, defined by an initial state, transition model,
 	and reward function. We also keep track of a gamma value, for use by
@@ -18,6 +14,7 @@ class MDPClass:
 		self.states = set()
 		self.list_actions = {(-1,0):'acima', (0,-1):'esquerda', (1,0):'abaixo', (0,1):'direita'}
 		self.state_actions = []
+		self.policy = {}
 
 
 	def R(self, state):
@@ -56,74 +53,25 @@ class GridMDPClass:
 		self.rows = len(grid)
 		self.cols = len(grid[0])
 		
-	# def T(self, state, action):
-	# 	if action == None:
-	# 		return [(0.0, state)]
-	# 	else:
-	# 		return [(0.8, self.go(state, action)),
-	# 				(0.1,grid self.go(state, turn_right(action))),
-	# 				(0.1, self.go(state, turn_left(action)))]
-
-	
-
 	def to_grid(self, mapping):
 		"""Convert a mapping from (x, y) to v into a [[..., v, ...]] grid."""
-		return list(reversed([[mapping.get((x,y), None)
-							   for x in range(self.cols)]
-							  for y in range(self.rows)]))
+		grid_pi = []
+		line_pi = []
+		for x in range(self.rows):
+			for y in range(self.cols):
+				if self.grid[x][y] is None:
+					line_pi.append('#')
+				elif self.grid[x][y] == 10:
+					line_pi.append('0')
+				elif self.grid[x][y] == -10:
+					line_pi.append('&')
+				else:
+					line_pi.append(mapping.get((x,y)))
+			grid_pi.append(line_pi)
+			line_pi = []
+
+		return grid_pi
 
 	def to_arrows(self, policy):
 		chars = {(0, 1):'>', (-1, 0):'^', (0, -1):'<', (1, 0):'v', None: '.'}
 		return self.to_grid(dict([(s, chars[a]) for (s, a) in policy.items()]))
-
-
-def value_iteration(mdp, epsilon=0.001):
-	"Solving an MDP by value iteration. [Fig. 17.4]"
-	U1 = dict([(s, 0) for s in mdp.states])
-	R, T, gamma = mdp.R, mdp.T, mdp.gamma
-	while True:
-		U = U1.copy()
-		delta = 0
-		for s in mdp.states:
-			U1[s] = R(s) + gamma * max([sum([p * U[s1] for (p, s1) in T(s, a)])
-										for a in mdp.actions(s)])
-			delta = max(delta, abs(U1[s] - U[s]))
-		if delta < epsilon * (1 - gamma) / gamma:
-			 return U
-
-def best_policy(mdp, U):
-	"""Given an MDP and a utility function U, determine the best policy,
-	as a mapping from state to action. (Equation 17.4)"""
-	pi = {}
-	for s in mdp.states:
-		pi[s] = argmax(mdp.actions(s), lambda a:expected_utility(a, s, U, mdp))
-	return pi
-
-def expected_utility(a, s, U, mdp):
-	"The expected utility of doing a in state s, according to the MDP and U."
-	return sum([p * U[s1] for (p, s1) in mdp.T(s, a)])
-
-
-def policy_iteration(mdp):
-	"Solve an MDP by policy iteration [Fig. 17.7]"
-	U = dict([(s, 0) for s in mdp.states])
-	pi = dict([(s, random.choice(mdp.actions(s))) for s in mdp.states])
-	while True:
-		U = policy_evaluation(pi, U, mdp)
-		unchanged = True
-		for s in mdp.states:
-			a = argmax(mdp.actions(s), lambda a: expected_utility(a,s,U,mdp))
-			if a != pi[s]:
-				pi[s] = a
-				unchanged = False
-		if unchanged:
-			return pi
-
-def policy_evaluation(pi, U, mdp, k=20):
-	"""Return an updated utility mapping U from each state in the MDP to its
-	utility, using an approximation (modified policy iteration)."""
-	R, T, gamma = mdp.R, mdp.T, mdp.gamma
-	for i in range(k):
-		for s in mdp.states:
-			U[s] = R(s) + gamma * sum([p * U[s] for (p, s1) in T(s, pi[s])])
-	return U
--- tp2/src/QLearning.py
+++ tp2/src/QLearning.py
@@ -9,30 +9,83 @@ class QLearningClass:
 		gridMDP = GridMDPClass(grid, terminals)
 		for x in range(gridMDP.rows):
 			for y in range(gridMDP.cols):
-				mdp.reward[x, y] = grid[x][y]
-				mdp.state_actions.append((x,y,'acima'))
-				mdp.state_actions.append((x,y,'abaixo'))
-				mdp.state_actions.append((x,y,'direita'))
-				mdp.state_actions.append((x,y,'esquerda'))
 				if grid[x][y] is not None:
 					mdp.states.add((x, y))
-		Q = dict([(s_a, 0) for s_a in mdp.state_actions])
-		current_state = random.sample(mdp.states,1)[0]
-		print(current_state)
-		action = random.choice(list(mdp.actions(current_state).keys()))
-		print(action)
+					mdp.reward[x, y] = grid[x][y]
+					mdp.state_actions.append((x,y,'acima'))
+					mdp.state_actions.append((x,y,'abaixo'))
+					mdp.state_actions.append((x,y,'direita'))
+					mdp.state_actions.append((x,y,'esquerda'))
 
-		# while action is not None:
-		next_state = mdp.go(current_state,action)
-		print(next_state)
+
+		Q = dict([(s_a, 0) for s_a in mdp.state_actions])
+
+		# total_reward = 0
+		episode = 0
+		episilon = 0.009
+		policy = []
+
+
+		current_state = random.sample(mdp.states,1)[0]
+		# print("Episode: ", episode)
+		
+		for i in range(N):
+			print(current_state)
+		
+			while current_state in mdp.terminals:
+				# print("Total_reward: ", total_reward)
+				episode += 1
+				# total_reward = 0
+				alpha -= episilon
+				# episilon *= 1/i+1
+				# print("Episode: ", episode)
+				current_state = random.sample(mdp.states,1)[0]
+				# print("New random state: ", current_state)
+			
+			action = random.choice(list(mdp.actions(current_state).keys()))
+			# print("Action: ", action)
+
+			next_state = mdp.go(current_state,action)
+			# print("Next state: ", next_state)
+			# total_reward += mdp.R(next_state)
 
 		# for a in list(mdp.list_actions.values()):
 		# 	print(Q[current_state + (a,)])
+			Q[current_state + (mdp.list_actions[action],)] = (1-alpha)*Q[current_state + (mdp.list_actions[action],)] + alpha*(mdp.R(next_state) + gamma*max([Q[current_state + (a,)] for a in list(mdp.list_actions.values())]))
+			current_state = next_state
+
+		with open("q.txt", 'w') as f:
+		    for key, value in Q.items():
+		    	if (key[0],key[1]) not in mdp.terminals:
+		        	f.write('%s,%s\n' % (str(key).strip('()').replace("'","").replace(" ", ""), value))
+
+		#policy
+
+		maxi = 0
+		mini = 0
+		for state in mdp.states:
+
+			maxi = max([Q[state + (a,)] for a in list(mdp.list_actions.values())])
+			mini = min([Q[state + (a,)] for a in list(mdp.list_actions.values())])
+
+			if maxi != 0 and maxi != mini:
+				policy.append(list(Q.keys())[list(Q.values()).index(maxi)])
+			else:
+				policy.append(state + (None,))
+
+		# print(policy)
+
+		for state_action in policy:
+			mdp.policy.update({(state_action[0],state_action[1]): list(mdp.list_actions.keys())[list(mdp.list_actions.values()).index(state_action[2])]})
 		
+		with open("pi.txt", "w") as pi:
+
+			# print(gridMDP.to_arrows(mdp.policy))
+			for item in gridMDP.to_arrows(mdp.policy):
+				pi.write("%s\n" % str(item).strip("[]").replace("'","").replace(", ",""))
+
 		
-		Q[current_state + (mdp.list_actions[action],)] = (1-alpha)*Q[current_state + (mdp.list_actions[action],)] + alpha*(mdp.R(next_state) + gamma*max([Q[current_state + (a,)] for a in list(mdp.list_actions.values())]))
-		
-		print(Q[current_state + (mdp.list_actions[action],)])
+
 		# pi = dict([(s, random.choice(mdp.actions(s))) for s in mdp.states])
 		
 
