6cca429106906341c942df92b487e89e7668628e
Romeu <romeu.oliveira@powerlogic.com.r>
2018-07-08 01:23:13 -0300

    Finalizado

--- tp2/src/Main.py
+++ tp2/src/Main.py
@@ -1,11 +1,14 @@
 from BuildGrid import BuildGridClass
 from QLearning import *
 import sys
+import time
 
 class MainClass:
 	
+	start_time = time.time()
 	buildGrid = BuildGridClass()
 	q = QLearningClass()
 	
 	grid, terminals = buildGrid.build_grid(sys.argv[1])
 	q.run(grid, terminals, float(sys.argv[2]), float(sys.argv[3]), int(sys.argv[4]))
+	# print(time.time() - start_time)
